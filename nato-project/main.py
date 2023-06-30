54956baa479b7e83b8b7c127bd78e3c705c9350d
hadessharma <scsa990@gmail.com>
2023-06-30 13:03:11 +0530

    updated exceptional handling into nato-project

--- nato-project/main.py
+++ nato-project/main.py
@@ -1,14 +1,16 @@
-
+def output() -> None:
+    try:
+        inputword = input('Enter a word: ').upper()
+        output_list = [student_dict[letter] for letter in inputword]
+        print(output_list)
+    except KeyError:
+        print('Sorry! Only letters and alphabets please!')
+        output()
+        
+        
 with open(file='./nato-project/nato_phonetic_alphabet.csv', mode='r') as file:
     data = file.read().splitlines()
     
-
-#TODO 1. Create a dictionary in this format:
 student_dict = { array[0]:array[1] for array in ([i.split(',') for i in data]) if array[0] != 'letter'}
 
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-inputword = input('Enter a word: ').upper()
-output_list = [student_dict[letter] for letter in inputword]
-print(output_list)
+output()
