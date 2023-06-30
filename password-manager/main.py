3d25d5a0707db5365508a9faac3d02d96fef76fe
hadessharma <66834721+hadessharma@users.noreply.github.com>
2023-06-30 14:13:42 +0530

    Feature/update/password manager (#11)
    * updated file type to json and added relevant functions
    
    * updated default_user to default user, to be enterd by user'
    
    * added exception keyerror in search password
    
    * completed updated for password manager
    
    * replaced exceptional handling with if/else

--- password-manager/main.py
+++ password-manager/main.py
@@ -3,11 +3,14 @@ from tkinter import messagebox
 import pyperclip
 import random
 import string
+import json
 
-DEFAULT_USER = 'scsa'
+DEFAULT_USER = 'abcd@gmail.com'
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
+    
+    
     s_lower  = list(string.ascii_lowercase)
     s_upper  = list(string.ascii_uppercase)
     s_digits = list(string.digits)
@@ -43,6 +46,8 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
+    
+    
     # check if website or password is empty
     if len(entry_website.get()) == 0:
         messagebox.showerror(title='Website!', message='Please enter valid website.')
@@ -51,21 +56,48 @@ def save_password():
         messagebox.showerror(title='Password!', message='Please enter a valid password')
         return
     
+    new_data = {
+        entry_website.get() : {
+            'password': entry_pass.get(),
+            'user'    : entry_user.get(),
+        }
+    }
+    
     # checking if the details submitted is okay
     is_okay = messagebox.askokcancel(title=entry_website.get(), message=f'These are the details entered: \nEmail: {entry_user.get()}\nPassword: {entry_pass.get()}')
 
     if is_okay:   
-        # save password into the file pass.txt
-        with open('./password-manager/pass.txt', 'a') as file:
-            file.write(f'{entry_website.get()}|{entry_user.get()}|{entry_pass.get()}\n')
+        # save password into the file pass.json
+        try:
+            with open('./password-manager/pass.json', 'r') as data_file:
+                data = json.load(data_file)
+                data.update(new_data)
+        except: # when json file is empty
+            with open('./password-manager/pass.json', 'w') as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            with open('./password-manager/pass.json', 'w') as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:    
+            # clear fields once saved
+            entry_pass.delete(0, END)
+            entry_website.delete(0, END)
+            
+            # focus back to website entry
+            entry_website.focus()
     
-    # clear fields once saved
-    entry_pass.delete(0, END)
-    entry_website.delete(0, END)
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+
+def search_password():
     
-    # focus back to website entry
-    entry_website.focus()
     
+    with open('./password-manager/pass.json', 'r') as data_file:
+        data = json.load(data_file)
+        if data.get(entry_website.get()):
+            messagebox.showinfo(title='Password found', message= f'For {entry_website.get()},\n user: {data.get(entry_website.get()).get("user")}\n pass: {data.get(entry_website.get()).get("password")}')
+        else:
+            messagebox.showerror(title='Passowrd not found', message= f'No data found for {entry_website.get()}')
+            
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Password Manager')
@@ -80,11 +112,15 @@ canvas.grid(column=1, row=0)
 label_website = Label(text='Website:')
 label_website.grid(column=0, row=1)
 
-entry_website = Entry(width=35)
-entry_website.grid(column=1, row= 1, columnspan=2, sticky='EW')
+entry_website = Entry(width=21)
+entry_website.grid(column=1, row= 1, columnspan=1, sticky='EW')
 entry_website.focus()
-# email/username entry
 
+# search button
+button_search = Button(text='Search', command=search_password)
+button_search.grid(column=2, row=1, sticky='EW')
+
+# email/username entry
 label_user = Label(text='Email/Username:')
 label_user.grid(column=0, row=2)
 
@@ -93,7 +129,6 @@ entry_user.grid(column=1, row=2, columnspan=2, sticky='EW')
 entry_user.insert(0, DEFAULT_USER)
 
 # Password entry
-
 label_pass = Label(text='Password:')
 label_pass.grid(column=0, row=3)
 
@@ -101,12 +136,10 @@ entry_pass = Entry(width=21)
 entry_pass.grid(column=1, row=3, sticky='EW')
 
 # generate password button
-
 button_generate_pass = Button(text='Generate Password', width=15, command=generate_password)
 button_generate_pass.grid(column=2, row=3, sticky='EW')
 
 # add button
-
 button_add = Button(text='Add', width=36, command=save_password)
 button_add.grid(column=1, row=4, columnspan=2, sticky='EW')
 
