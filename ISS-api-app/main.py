090dc1606b8c5ce5c8ee798ae6430fd27659162e
Deep <dr4477@srmist.edu.in>
2023-08-18 21:12:29 +0530

    converted local time to utc

--- ISS-api-app/main.py
+++ ISS-api-app/main.py
@@ -1,10 +1,10 @@
 import requests
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timezone
 
 MY_LONGITUDE = 73.743202
 MY_LATITUDE  = 18.589800
-NOW = datetime.now()
+NOW = datetime.now(timezone.utc)
 
 # response = requests.get(url='http://api.open-notify.org/iss-now.json')
 # response.raise_for_status()
@@ -27,10 +27,13 @@ parameters = {
 response = requests.get(url='https://api.sunrise-sunset.org/json',params=parameters)
 response.raise_for_status()
 
-data    = pd.json_normalize(response.json()['results'])
-sunrise = data['sunrise'][0]
-sunset  = data['sunset'][0]
 
+data    = pd.json_normalize(response.json()['results'])
+sunrise = data['sunrise'][0].split('T')[1].split(':')[0]
+sunset  = data['sunset'][0].split('T')[1].split(':')[0]
+
+time_hour = NOW.hour
 
 print(sunrise)
 print(sunset)
+print(time_hour)
