023b203c1e2eff11f60a6885d4f12316fb4c1cb9
hadessharma <scsa990@gmail.com>
2023-08-21 15:01:53 +0530

    added base code

---
+++ weather-app/main.py
@@ -0,0 +1,18 @@
+import requests
+
+parameters = {
+    'lat'  : 18.580750,
+    'lon'  : 73.744610,
+    'appid': '84cfe15e61d71145d7601320c2ecff1a',
+    # 'q'    : 'London'
+}
+
+with requests.get(url='https://api.openweathermap.org/data/2.5/weather', params=parameters) as connection:
+    connection.raise_for_status()
+    
+    weather = connection.json()
+    if weather['weather'][0]['id'] > 700:
+        will_rain = False
+    else:
+        will_rain = True
+        
