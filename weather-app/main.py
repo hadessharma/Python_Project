a6afe7c22118318d097de8b48d8e20baf6f37be1
hadessharma <scsa990@gmail.com>
2023-08-22 14:19:11 +0530

    testing commint

--- weather-app/main.py
+++ weather-app/main.py
@@ -4,8 +4,10 @@ parameters = {
     'lat'  : 18.580750,
     'lon'  : 73.744610,
     'appid': '84cfe15e61d71145d7601320c2ecff1a',
-    # 'q'    : 'London'
+    # 'q'    : 'Pune'
 }
+
+will_rain = None
 
 with requests.get(url='https://api.openweathermap.org/data/2.5/weather', params=parameters) as connection:
     connection.raise_for_status()
